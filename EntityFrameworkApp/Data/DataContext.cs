fc914b0783afc087b8ab578e3bbd24d8ffe2b6d0
Matvey <sinema3d00@gmail.com>
2024-08-24 20:28:40 +0300

    making data context

---
+++ EntityFrameworkApp/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using EntityFrameworkApp.Model;
+using System.Data.Entity;
+
+namespace EntityFrameworkApp.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DataContext() { }
+    }
+}
