b927004a37831d755613760295dfaae38db0c619
Matvey <sinema3d00@gmail.com>
2024-08-25 20:55:25 +0300

    creating new interface

---
+++ EntityFrameworkApp/Interfaces/IAddress.cs
@@ -0,0 +1,18 @@
+﻿using EntityFrameworkApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkApp.Interfaces
+{
+    public interface IAddress
+    {
+        IEnumerable<Address> GetAll();
+        Address GetById(int id);
+        void Add(Address address);
+        void Update(Address address);
+        void Delete(int id);
+    }
+}
