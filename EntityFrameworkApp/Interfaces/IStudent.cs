cf6fc450a444017f4f837d384e211a307f6dd37e
Matvey <sinema3d00@gmail.com>
2024-08-25 17:37:58 +0300

    creating Interface

---
+++ EntityFrameworkApp/Interfaces/IStudent.cs
@@ -0,0 +1,15 @@
+﻿using EntityFrameworkApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkApp.Repositories
+{
+    internal interface IStudent
+    {
+        IEnumerable<Student> GetAll();
+        Student GetById(int id);
+    }
+}
