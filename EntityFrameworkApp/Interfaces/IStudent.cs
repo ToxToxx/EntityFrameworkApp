99bdc68f7de8eb2a4e3300516e214e5f126e7b5e
Matvey <sinema3d00@gmail.com>
2024-08-25 17:41:11 +0300

    made interface

--- EntityFrameworkApp/Interfaces/IStudent.cs
+++ EntityFrameworkApp/Interfaces/IStudent.cs
@@ -11,5 +11,9 @@ namespace EntityFrameworkApp.Repositories
     {
         IEnumerable<Student> GetAll();
         Student GetById(int id);
+        void Add(Student student);
+        void Update(Student student);
+        void Delete(int id);
+
     }
 }
