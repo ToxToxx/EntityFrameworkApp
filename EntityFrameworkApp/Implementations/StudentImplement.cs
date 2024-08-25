2fd3a4bb4cfa5388cfaef1240beaeded200047a5
Matvey <sinema3d00@gmail.com>
2024-08-25 17:41:17 +0300

    making implementation

---
+++ EntityFrameworkApp/Implementations/StudentImplement.cs
@@ -0,0 +1,16 @@
+﻿using EntityFrameworkApp.Data;
+using EntityFrameworkApp.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkApp.Implementations
+{
+    public class StudentImplement : IStudent
+    {
+        private readonly DataContext _context;
+
+    }
+}
