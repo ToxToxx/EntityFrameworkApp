389a3aff19532d4add89428a20efdb3c658a2bba
Matvey <sinema3d00@gmail.com>
2024-08-25 17:46:11 +0300

    made implementation of interface Istudent

---
+++ EntityFrameworkApp/Implementations/StudentRepository.cs
@@ -0,0 +1,53 @@
+﻿using EntityFrameworkApp.Data;
+using EntityFrameworkApp.Model;
+using EntityFrameworkApp.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkApp.Implementations
+{
+    public class StudentRepository : IStudent
+    {
+        private readonly DataContext _context;
+
+        public StudentRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Student> GetAll()
+        {
+            return _context.Students.ToList();
+        }
+
+        public Student GetById(int id)
+        {
+            return _context.Students.Find(id);
+        }
+
+        public void Add(Student student)
+        {
+            _context.Students.Add(student);
+            _context.SaveChanges();
+        }
+
+        public void Update(Student student)
+        {
+            _context.Entry(student).State = System.Data.Entity.EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var student = _context.Students.Find(id);
+            if(student != null)
+            {
+                _context.Students.Remove(student);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
