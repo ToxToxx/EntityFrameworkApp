0b89d2be27f145a7ea5a3b03bb57a651846bda1d
Matvey <sinema3d00@gmail.com>
2024-08-26 09:36:46 +0300

    separate student view model in class view model

---
+++ EntityFrameworkApp/ViewModel/StudentViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkApp.ViewModel
+{
+    public class StudentViewModel
+    {
+        public int IdStudent { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Patronymic { get; set; }
+        public string FullAddress { get; set; }
+    }
+}
