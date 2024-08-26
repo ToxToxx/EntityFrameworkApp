e2d37e566b69c13a70a0b9d7b4e195230b9c775d
Matvey <sinema3d00@gmail.com>
2024-08-26 10:12:20 +0300

    making in SQL procedure for adding students

--- EntityFrameworkApp/Data/DataContext.cs
+++ EntityFrameworkApp/Data/DataContext.cs
@@ -1,5 +1,7 @@
 ﻿using EntityFrameworkApp.Model;
+using System.Data;
 using System.Data.Entity;
+using System.Data.SqlClient;
 
 namespace EntityFrameworkApp.Data
 {
@@ -18,6 +20,31 @@ namespace EntityFrameworkApp.Data
                 .HasRequired(s => s.AddressObject)
                 .WithMany()
                 .HasForeignKey(s => s.AddressId);
+        }
+
+        public void CreateStudent(Student student, Address address)
+        {
+            var connection = Database.Connection;
+            using (var command = connection.CreateCommand())
+            {
+                command.CommandText = "CreateStudent";
+                command.CommandType = CommandType.StoredProcedure;
+                command.Parameters.Add(new SqlParameter("@LastName", student.LastName));
+                command.Parameters.Add(new SqlParameter("@FirstName", student.FirstName));
+                command.Parameters.Add(new SqlParameter("@Patronymic", student.Patronymic));
+                command.Parameters.Add(new SqlParameter("@Name", address.Name));
+                command.Parameters.Add(new SqlParameter("@City", address.City));
+                command.Parameters.Add(new SqlParameter("@State", address.State));
+                command.Parameters.Add(new SqlParameter("@ZipCode", address.ZipCode));
+
+
+                if(connection.State != ConnectionState.Open)
+                {
+                    connection.Open();
+                }
+
+                command.ExecuteNonQuery();
+            }
         }
     }
 }
